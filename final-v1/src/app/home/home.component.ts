51c7bcd4c3c2a61dbb2f1839e0baf80684b1e603
sngalamou <42877564+sngalamou@users.noreply.github.com>
2019-12-12 13:18:56 -0600

    added servlet

--- final-v1/src/app/home/home.component.ts
+++ final-v1/src/app/home/home.component.ts
@@ -7,44 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  word: string = "";
-  trans: string = "";
+  static trans: string;
   cors: string = "https://cors-anywhere.herokuapp.com/";
+  static word: string;
+  word1: string;
+  trans1: string;
+  static dataJson = {"sourceText": " goodmorning "};
   
 
   constructor() { }
 
   ngOnInit() {
-
-  }
-
-
-  initApplication(word,trans) {
-    var link2: string = "https://microsoft-azure-translation-v1.p.rapidapi.com/translate?from=en&to=es&text=";
+    var word1;
     var link1: string = this.cors + "https://www.dictionary.com/e/word-of-the-day/";
     var indexRequest = new XMLHttpRequest();
     indexRequest.open('GET', link1);
     indexRequest.onload = function() {
         var res: string[] = (indexRequest.responseText).split(" ", 200);
-        console.log(res[179]);
-        word = res[179];
-        console.log(word);
+        word1 = res[179];
+        HomeComponent.word = word1;
 
-        link2 += word;
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
-        xhr.open("GET", link2 );
-        xhr.setRequestHeader("x-rapidapi-host", "microsoft-azure-translation-v1.p.rapidapi.com");
-        xhr.setRequestHeader("x-rapidapi-key", "0d8ce3de65mshafcf99926226a2cp1f78e5jsn5f88341225c9");
-        xhr.setRequestHeader("accept", "application/json");
-
-        
-        xhr.onload = function () {
-                console.log("Done");
-                console.log(this.responseText);
-                trans = this.responseText;
+        HomeComponent.dataJson.sourceText = word1
+      var data = JSON.stringify(HomeComponent.dataJson);
+      
+      var xhr = new XMLHttpRequest();
+      xhr.withCredentials = true;
+      
+      xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+          var response = (this.responseText);
+          var res = response.split("\"");
+          console.log(res[3]);
+          HomeComponent.trans = res[3];
         }
+      });
+      
+      xhr.open("POST", "https://my-translator-pro.p.rapidapi.com/api/translate");
+      xhr.setRequestHeader("x-rapidapi-host", "my-translator-pro.p.rapidapi.com");
+      xhr.setRequestHeader("x-rapidapi-key", "0d8ce3de65mshafcf99926226a2cp1f78e5jsn5f88341225c9");
+      xhr.setRequestHeader("targetlanguage", "es");
+      xhr.setRequestHeader("content-type", "application/json");
+      xhr.setRequestHeader("accept", "application/json");
+      
+      xhr.send(data);
+        
     }
     indexRequest.send();
+      
+  }
+
+  
+
+  posting() {
+    this.word1 = HomeComponent.word;
+    this.trans1 = HomeComponent.trans;
   }
 }
